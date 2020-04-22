d0f7684158a3b36c8f38b5bcf64b0b620238f025
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-04-22 15:51:38 +0200

    add js for arrow imgs

--- public/js/script.js
+++ public/js/script.js
@@ -7,15 +7,31 @@ var minuten = document.querySelector('#minuten')
 var submitButton = document.querySelector('button')
 
 l.focus();
+
+function arrowLeft () {
+    let leftArrow = document.querySelector('#left-con')
+    leftArrow.classList.add("active")
+    let rightArrow = document.querySelector('#right-con')
+    rightArrow.classList.remove("active")
+}
+function arrowRight () {
+    let rightArrow = document.querySelector('#right-con')
+    rightArrow.classList.add("active")
+    let leftArrow = document.querySelector('#left-con')
+    leftArrow.classList.remove("active")
+    
+}
 //destinationTab
 l.addEventListener('keydown', function(lTab){
     // arrow left
     if (lTab.which == 37) {
         document.querySelector('button').focus();
+        arrowLeft()
         return false;
     }//arrowright
      else if (lTab.which === 39) {
         document.getElementById("destination").focus();
+        arrowRight()
         return false;
     }
 })
@@ -25,6 +41,7 @@ destination.addEventListener('keydown', function(destinationTab){
     // arrow left
     if (destinationTab.which == 37) {
         document.getElementById("location").focus();
+        arrowLeft()
         return false;
     }//arrowright
      else if (destinationTab.which === 39) {
@@ -37,10 +54,12 @@ day.addEventListener('keydown', function(dayTab){
     // arrow left
     if (dayTab.which == 37) {
         document.getElementById("destination").focus();
+        arrowLeft()
         return false;
     }//arrowright
      else if (dayTab.which === 39) {
         document.getElementById("month").focus();
+        arrowRight()
         return false;
     }
 })
@@ -48,10 +67,12 @@ month.addEventListener('keydown', function(monthTab){
     // arrow left
     if (monthTab.which == 37) {
         document.getElementById("day").focus();
+        arrowLeft()
         return false;
     }//arrowright
      else if (monthTab.which === 39) {
         document.getElementById("hour").focus();
+        arrowRight()
         return false;
     }
 })
@@ -59,10 +80,12 @@ hour.addEventListener('keydown', function(hourTab){
     // arrow left
     if (hourTab.which == 37) {
         document.getElementById("month").focus();
+        arrowLeft()
         return false;
     }//arrowright
      else if (hourTab.which === 39) {
         document.getElementById("minuten").focus();
+        arrowRight()
         return false;
     }
 })
@@ -70,10 +93,12 @@ minuten.addEventListener('keydown', function(minutenTab){
     // arrow left
     if (minutenTab.which == 37) {
         document.getElementById("hour").focus();
+        arrowLeft()
         return false
     }//arrowright
      else if (minutenTab.which === 39) {
         document.querySelector('button').focus();
+        arrowRight()
         return false;
     }
 })
@@ -82,107 +107,13 @@ submitButton.addEventListener('keydown', function(submitButtonTab){
     // arrow left
     if (submitButtonTab.which == 37) {
         document.getElementById("minuten").focus();
+        arrowLeft()
         return false
     }//arrowright
      else if (submitButtonTab.which === 39) {
         document.getElementById("location").focus();
+        arrowRight()
         return false;
     }
 })
 
-//daytabs
-// day.addEventListener('keydown', function(dayTab){
-//     // a
-//     if (dayTab.which === 65) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     }//s
-//      else if (dayTab.which === 83) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     }//d
-//     else if (dayTab.which === 68) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     } //f
-//     else if (dayTab.which === 70) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     }
-//     // h
-//     else if (dayTab.which === 72) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }//j
-//      else if (dayTab.which === 74) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }//k
-//     else if (dayTab.which === 75) {
-//         document.getElementById("month").focus();
-//         return false;
-//     } //l
-//     else if (dayTab.which === 76) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }
-// })
-
-// //daytabs
-// day.addEventListener('keydown', function(dayTab){
-//     // a
-//     if (dayTab.which === 65) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     }//s
-//      else if (dayTab.which === 83) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     }//d
-//     else if (dayTab.which === 68) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     } //f
-//     else if (dayTab.which === 70) {
-//         document.getElementById("destination").focus();
-//         return false;
-//     }
-//     // h
-//     else if (dayTab.which === 72) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }//j
-//      else if (dayTab.which === 74) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }//k
-//     else if (dayTab.which === 75) {
-//         document.getElementById("month").focus();
-//         return false;
-//     } //l
-//     else if (dayTab.which === 76) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }
-// })
-
-// s button
-//day 
-// const day = document.getElementById("day")
-// day.addEventListener('keydown', function(e2){
-//     if (e2.which === 83) {
-//         document.getElementById("month").focus();
-//         return false;
-//     }
-    
-// })
-// D button
-//day 
-
-// H button
-
-
-// var dt = new Date();
-// dt.setHours( dt.getHours() + 1 );
-// document.write( dt );
-// console.log(dt)
